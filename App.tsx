1a9ea9460996142648464ad49408cacec5e2a768
Ben Schlegel <ben5.schlegel@gmail.com>
2021-10-11 14:44:52 +0200

    Set up basic project structure

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
+import { registerRootComponent } from 'expo';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default function App(): React.ReactElement {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>

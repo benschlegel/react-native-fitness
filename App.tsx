ca11f9bbe847fea1902768d1365fafb155623966
Ben Schlegel <ben5.schlegel@gmail.com>
2021-10-14 17:17:59 +0200

    Add icons to tab navigator, fix screens

--- App.tsx
+++ App.tsx
@@ -1,13 +1,12 @@
-import { registerRootComponent } from 'expo';
 // import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
 import { Overview } from './src/Overview';
-import { Onboarding } from './src/Onboarding';
 import { WorkoutPlanner } from './src/WorkoutPlanner';
 import type { Routes } from './src/Routes';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import Ionicons from ;
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 export const Tab = createBottomTabNavigator<Routes>();
 
@@ -15,10 +14,29 @@ export default function App(): React.ReactElement {
   // const isOnboarding = useState(false);
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{ headerShown: false }} initialRouteName={'Overview'}>
+      <Tab.Navigator
+        initialRouteName={'Overview'}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName = 'asd';
+
+            if (route.name === 'Overview') {
+              iconName = focused ? 'fitness-outline' : 'fitness';
+            } else if (route.name === 'WorkoutPlanner') {
+              iconName = focused ? 'reader-outline' : 'reader';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          // TODO: change colors to match scheme
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+          headerShown: false,
+        })}>
         <Tab.Screen name="Overview" component={Overview} />
-        <Tab.Screen name="Onboarding" component={Onboarding} />
-        <Tab.Screen name="WorkoutPlanner" component={WorkoutPlanner} />
+        {/* <Tab.Screen name="Onboarding" component={Onboarding} /> */}
+        <Tab.Screen name="WorkoutPlanner" component={WorkoutPlanner} options={{ tabBarLabel: 'Plan workout' }} />
       </Tab.Navigator>
     </NavigationContainer>
   );
